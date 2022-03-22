766f2870fac5f3140a77ade1e629dc52714b5396
kadnur <nur.bakirci@gmail.com>
2022-03-22 22:06:20 +0100

    Deleted answer 2

--- EasyQuestions/02_ValidateSubsequence/ValidateSubsequence.py
+++
@@ -1,9 +0,0 @@
-def array_1(array,sequence):
-  j=0
-  for num in array:
-    if j == len(sequence):
-      break
-    if sequence[j] == num:
-      j += 1
-  return j == len(sequence)
-print(array_1([6,-3,8,10,-2,5],[6,8,10]))
